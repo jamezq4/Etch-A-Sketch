c64a0a51bd07078a470fc10c1d926a3d358eb783
jamezq <jamezquita240@gmail.com>
2023-01-06 08:47:55 -0600

    Define changeTilecolor function

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
+function changeTileColor(e) 
+{
+    if (e.target.className === 'column')
+    {
+        e.target.style.backgroundColor = globalRGBValue;
+    }
+
+    //CODE FOR RANDOM COLORED TILES
+
+    //let idValue = Number(e.target.id)
+
+    //if (idValue < 10)
+    //{
+        //idValue++
+        //e.target.id = idValue.toString()
+    //}
+
+    //let maxRGBValue = Math.round(255/ idValue)
+    //let red;
+    //let green;
+    //let blue;
+
+    //Credit to assembly wizard on stack overflow (How do you get random RGB in javascript)
+    //const randomColor = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+
+    // if (idValue < 10)
+    //{
+        //red =  randomColor(30, maxRGBValue)
+        //green = randomColor(30, maxRGBValue)
+        //blue = randomColor(30, maxRGBValue)
+    //}
+    //else
+    //{
+        //red = 0
+        //green = 0
+        //blue = 0
+        //e.target.style.borderColorc = 'white'
+    //}
+
+    //const rgbValue = `rgb(${red}, ${green}, ${blue})
+
+    //e.target.style.backgroundColor = globalRGBValue
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Main Body
 let clicked = true;
 let globalRGBValue = '';
